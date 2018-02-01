3278b26850d57fac04cbe8ef55e78898eaf63567
Matheus Inácio BS <matheus.bs@ic.ufal.br>
2018-02-01 16:36:45 -0300

    OperationTest

--- code/src/test/java/com/laccan/app/calculator/OperationTest.java
+++ code/src/test/java/com/laccan/app/calculator/OperationTest.java
@@ -5,20 +5,34 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class OperationTest {
+    int a = 10, b = 10;
 
     @Test
     public void sum() {
+        int result = a + b;
+        int test = (int) Operation.sum(a, b);
+        assertEquals(result, test);
+
     }
 
     @Test
     public void subtract() {
+        int result = a - b;
+        int test = (int) Operation.subtract(a, b);
+        assertEquals(result, test);
     }
 
     @Test
     public void division() {
+        int result = a / b;
+        int test = (int) Operation.division(a, b);
+        assertEquals(result, test);
     }
 
     @Test
     public void multiply() {
+        int result = a * b;
+        int test = (int) Operation.multiply(a, b);
+        assertEquals(result, test);
     }
 }
