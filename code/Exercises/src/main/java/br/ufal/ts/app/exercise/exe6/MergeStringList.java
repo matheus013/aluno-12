8780c386df2c1eaef065c8a496037bc300b91ec0
Matheus <matheus.bs@ic.ufal.br>
2018-03-08 13:52:22 -0300

    update

--- code/Exercises/src/main/java/br/ufal/ts/app/exercise/exe6/MergeStringList.java
+++ code/Exercises/src/main/java/br/ufal/ts/app/exercise/exe6/MergeStringList.java
@@ -7,7 +7,9 @@ public class MergeStringList {
     public String get(List<String> list, int start, int end, String separator) {
         FilterPosition filterPosition = new FilterPosition(list);
         List<String> partialResult = list.stream()
-                .filter(v -> filterPosition.setPosition(start, end).accept(v))
+                .filter(v -> filterPosition
+                        .setPosition(start, end)
+                        .accept(v))
                 .collect(Collectors.toList());
         String result = String.join(separator, partialResult);
 
--- code/Exercises/src/main/java/br/ufal/ts/app/main/ExercisesMain.java
+++ code/Exercises/src/main/java/br/ufal/ts/app/main/ExercisesMain.java
@@ -7,15 +7,14 @@ import br.ufal.ts.app.exercise.exe5.LowercaseList;
 import br.ufal.ts.app.exercise.exe6.MergeStringList;
 import br.ufal.ts.utils.Helper;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 public class ExercisesMain {
-    public static void main(String[] args) throws IOException {
-        List<String> listEx1 = Arrays.asList("alpha", "bravo", "charlie", "delta", "echo", "foxtrot");
-        MapByFirst map = new MapByFirst(listEx1);
-        List<String> list = Arrays.asList("The", "Quick", "BROWN", "Fox", "Jumped", "Over", "The", "LAZY", "DOG");
+    public static void main(String[] args) {
+        List<Integer> listTwo = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<String> list = Arrays.asList("alpha", "bravo", "charlie", "delta", "echo", "foxtrot");
+        MapByFirst map = new MapByFirst(list);
         FiltratedListVowel filtratedListVowel = new FiltratedListVowel();
         FiltratedListPairLength filterByPairLength = new FiltratedListPairLength();
         LowercaseList lowercaseList = new LowercaseList();
@@ -25,11 +24,12 @@ public class ExercisesMain {
         Helper.printList(map.getAll());
 
         System.out.println("Exercise 2");
-        Helper.printList(filtratedListVowel.get(listEx1));
+        Helper.printList(filtratedListVowel.get(list));
 
         System.out.println("Exercise 3");
-        Helper.printList(filterByPairLength.get(listEx1));
+        Helper.printList(filterByPairLength.get(list));
 
+        list = Arrays.asList("The", "Quick", "BROWN", "Fox", "Jumped", "Over", "The", "LAZY", "DOG");
         System.out.println("Exercise 5");
         Helper.printList(lowercaseList.get(list));
 
@@ -37,6 +37,12 @@ public class ExercisesMain {
         System.out.println("Exercise 6");
         System.out.println(mergeStringList.get(list, 2, 4, "-"));
 
+        Runnable runnable = () -> {
+            System.out.println("Exercise 4");
+            listTwo.stream().forEach(System.out::println);
+        };
+
+        new Thread(runnable).start();
 
     }
 }
